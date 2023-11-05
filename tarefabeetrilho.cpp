444fb23b028f28b0da149bf23140d66cb976a6b6
Bell Gemim <giancarlogemim@gmail.com>
2023-11-05 20:49:51 -0300

    aa
    a

--- tarefabeetrilho.cpp
+++ tarefabeetrilho.cpp
@@ -2,44 +2,61 @@
  
 using namespace std;
  
- typedef struct na{
-     int info;
-     na *prox;
-     na *ante;
- } no;
- 
-  no* push(int x, no* L){
-     no *p=NULL, *AUX = NULL;
-     AUX = L->ante;
-     p = new no;
-     p->info = x;
-     if(L == NULL){
-        p->ante= p;
-        p->prox= p;
-        L = p; 
-        return L;
-     }
-     else {
-       p->ante = L->ante;
-       p->prox = L;
-       AUX->prox=p;
-       L->ante = p;
-       return L;
-     }
- }
- 
- no* pop(int *x, no *L){
-    no *p = NULL, *AUX= NULL;
-    AUX = L->ante;
-    *x = AUX->info;
-    L->ante = AUX->ante;
-    AUX = AUX->ante;
-    AUX->prox = L; 
 
- }
 
+ 
 int main() {
- 
-   
+   int x, j, *L, *M, *N, O[4], k, z, b, i;
+   string a;
+   do{
+    cin>>x;
+    if(x!= 0){
+        do{
+            cin>>a;
+            if(a!="0"){
+                L = new int[x];
+                M = new int[x];
+                N = new int[x];
+                z=0;
+                k=0;
+                b=0;
+                for(i=0;a[i]!="/0";i++){
+                    if((a[i-1]==" ")||(i==0)){
+                        z=i;
+                    }
+                    O[k]=a[i];
+                    k++;
+                    if((a[i+1]==" ")||(a[i+1]=="/0")){
+                        int d=0;
+                        for(int c=k;c>0;c--){
+                            N[b]+= O[d]*10^c;
+                        }
+                        b++;
+                    }
+                    
+                }
+                j=0;
+                for(i=0;i<x;i++){
+                    L[i]=i+1;
+                }
+                for(i=x;i>0;i--){
+                    M[j]=i;
+                    j++;
+                }
+                for(i=0;(i<x)||(j<0);i++){
+                    if((L[i]!=N[i])&&(L[j]!=N[i])){
+                        i=x+1;
+                        cout<<"No"<<endl;
+                    }
+                    j--;
+                }
+                if(i!=x+1){
+                    cout<<"Yes"<<endl;
+                }
+                
+            }
+        }while(a!="0");
+    }
+   }while(x!=0);
     return 0;
 }
