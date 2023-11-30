46c1c4ecb2f6728ab28f1203985ed7c5f4b4d6d2
BellGemim <giancarlogemim@gmail.com>
2023-11-30 11:30:09 -0300

    dependencias

--- integrador/lib/main.dart
+++ integrador/lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home : Paglogin(),
+      home : Pagmain(),
       routes: {
         '/pagmain' :(context) => Pagmain(),
         '/paglogin' :(context) => Paglogin(),
