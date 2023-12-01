d272c45b87ce3f90f265ca9ac3d22df0c2b20d6b
BellGemim <giancarlogemim@gmail.com>
2023-12-01 09:51:38 -0300

    aa

--- integrador/lib/main.dart
+++ integrador/lib/main.dart
@@ -1,15 +1,20 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:integrador/pages/paglogin.dart';
 import 'package:integrador/pages/pagmain.dart';
 import 'package:integrador/pages/pagsingup.dart';
-//import 'package:firebase_core/firebase_core.dart';
 
-void main() /*async*/ {
- // WidgetsFlutterBinding.ensureInitialized();
+Future main() async{
+ WidgetsFlutterBinding.ensureInitialized();
 
-  //await Firebase.initializeApp();
+if(kIsWeb){
+  await Firebase.initializeApp(options: FirebaseOptions(apiKey: "AIzaSyDC1cckBjwDKEiFPNzTdOrPGL_4MLlcBg4", appId: "1:673412955638:web:1c0e13e46a0410048b5bd7", messagingSenderId:"673412955638", projectId: "integras-43a6d"));
+}
+
+  Firebase.initializeApp();
 
   runApp(const MyApp());
 }
